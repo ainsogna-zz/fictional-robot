f75344ef76b4856c1a1eaec3c34d7aafb10a7471
ainsogna <ainsogna@gmail.com>
2020-02-08 13:16:56 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
+import Slider from 'react-slider-simple';
+import Switch from 'react-toggle-switch';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      percent: 50,
+      switched: false
+    };
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  toggleSwitch = () => {
+    this.setState(prevState => {
+      return {
+        switched: !prevState.switched
+      };
+    });
+  };
+
+  handleClick() {
+    fetch("https://5tmuubkncf.execute-api.us-west-2.amazonaws.com/test/pets?foo=17")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          window.alert(result);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          console.log(error);
+        }
+      )
+  }
+
+  onChange = (percent) => {
+    this.setState({ percent });
+  }
+
+  onDone = (percent) => {
+    this.setState({ percent });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          ¯\_(ツ)_/¯
+        </header>
+        
+        <div>
+          <span>Oat milk?</span>
+          <span>
+            <Switch
+              onClick={this.toggleSwitch}
+              on={this.state.switched}
+            />
+          </span>
+        </div>
+        {/* <Slider
+            value={this.state.percent}
+            onChange={this.onChange}
+            onDone={this.onDone}
+          /> */}
+        <button className="App-button" onClick={this.handleClick}>
+          Go!
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
