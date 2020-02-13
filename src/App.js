a8fdfc2f1d15bdf02caac841e60976626075015e
ainsogna <ainsogna@gmail.com>
2020-02-12 23:36:20 -0800

    inputs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Switch from 'react-toggle-switch';
+// import Switch from 'react-toggle-switch';
 import Tabletop from 'tabletop';
 import './App.css';
 
@@ -9,11 +9,14 @@ class App extends React.Component {
     this.state = { 
       percent: 50,
       switched: false,
+      requireApplePay: false,
+      orderAhead: false,
       data: []
     };
 
     // This binding is necessary to make `this` work in the callback
     this.handleClick = this.handleClick.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   toggleSwitch = () => {
@@ -26,9 +29,23 @@ class App extends React.Component {
 
   handleClick() {
     const data = this.state.data;
-    const index = Math.floor(Math.random() * data.length);
-    const row = data[index];
-    const prompt = row.Name + " at " + row.Location + "\n\nReviews:\n" + row.Reviews;
+
+    var results = [];
+    for (var row of data) {
+      console.log(row["Apple Pay?"]);
+      const b1 = !this.state.requireApplePay || (this.state.requireApplePay && row['Apple Pay?'] === '👍');
+      const b2 = !this.state.orderAhead || (this.state.orderAhead && row['Order Ahead?'] === '👍');
+
+      if (b1 && b2) {
+        results.push(row);
+      }
+    }
+
+    console.log(results);
+
+    const index = Math.floor(Math.random() * results.length);
+    const resultRow = results[index];
+    const prompt = resultRow.Name + " at " + resultRow.Location + "\n\nReviews:\n" + resultRow.Reviews;
     window.alert(prompt);
   }
 
@@ -38,6 +55,7 @@ class App extends React.Component {
       // copy: 1-kaR3hI338g-9v5ona8guM-Q79KVXO3OBEq7xKT_miU
       key: '14vqSXVmuBh58UCQg7ar_SeHd2zehWLz2mCt8thaWKGw',
       callback: googleData => {
+        console.log(googleData);
         this.setState({ data: googleData });
       },
       simpleSheet: true
@@ -52,29 +70,47 @@ class App extends React.Component {
     this.setState({ percent });
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.checked;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           ¯\_(ツ)_/¯
         </header>
-        
+      
         <div>
-          <span>Oat milk?</span>
-          <span>
-            <Switch
-              onClick={this.toggleSwitch}
-              on={this.state.switched}
-            />
-          </span>
+          <label>
+            Require Apple Pay?
+            <input
+              name="requireApplePay"
+              type="checkbox"
+              checked={this.state.requireApplePay}
+              onChange={this.handleInputChange} />
+          </label>
         </div>
-        {/* <Slider
-            value={this.state.percent}
-            onChange={this.onChange}
-            onDone={this.onDone}
-          /> */}
+
+        <div>
+          <label>
+            Order Ahead?
+            <input
+              name="orderAhead"
+              type="checkbox"
+              checked={this.state.orderAhead}
+              onChange={this.handleInputChange} />
+          </label>
+        </div>
+
         <button className="App-button" onClick={this.handleClick}>
-          Go!
+          ☕
         </button>
       </div>
     );
